4453961f657b8d9e6ebab259c75bd385a56ee16f
brunsst <91964829+brunsst@users.noreply.github.com>
2023-05-16 17:08:18 +0200

    Add files via upload

--- build_mbsoptflow.sh
+++ build_mbsoptflow.sh
@@ -6,6 +6,8 @@ CUDA_COMPUTECAPABILITY=70
 #CUDA_COMPUTECAPABILITY=70
 NVCC_PATH="/usr/local/cuda-10.0/bin/nvcc"
 
+BUILD_BINARY=true
+UNZIP_DEMOS=false
 #########################################
 #########################################
 
@@ -28,7 +30,7 @@ function cuda_compile()
     $NVCC_PATH --disable-warnings -O3 -Xcompiler -fopenmp -std=c++11 --compile --relocatable-device-code=false -gencode arch=compute_$CUDA_COMPUTECAPABILITY,code=compute_$CUDA_COMPUTECAPABILITY -gencode arch=compute_$CUDA_COMPUTECAPABILITY,code=sm_$CUDA_COMPUTECAPABILITY  -x cu -o  "Build/"$filename".o" $subdir"/"$filename".cu"
 }
 
-if test $CUDA_COMPUTECAPABILITY != 0 && test $CUDA_COMPUTECAPABILITY != 00
+if $BUILD_BINARY && test $CUDA_COMPUTECAPABILITY != 0 && test $CUDA_COMPUTECAPABILITY != 00
 then
     echo "building cudaWBBOptFlow_v0.2"
     echo "--------------------------------------------------"
@@ -61,7 +63,20 @@ then
     $NVCC_PATH --cudart static --relocatable-device-code=false -gencode arch=compute_$CUDA_COMPUTECAPABILITY,code=compute_$CUDA_COMPUTECAPABILITY -gencode arch=compute_$CUDA_COMPUTECAPABILITY,code=sm_$CUDA_COMPUTECAPABILITY -link -o  "mbsoptflow"  ./Build/main.o  ./Build/farid_kernels.o ./Build/optflow_cpu2d.o ./Build/optflow_cpu3d.o ./Build/optflow_gpu2d.o ./Build/optflow_gpu2d_reshape.o ./Build/optflow_gpu3d.o ./Build/optflow_gpu3d_reshape.o  ./Build/pyramid.o ./Build/resampling.o  ./Build/histomatching.o  ./Build/cornerdetection_cpu.o  ./Build/auxiliary.o ./Build/derivatives.o ./Build/eig3.o ./Build/filtering.o ./Build/hdcommunication.o ./Build/histogram.o  ./Build/register_correlationwindow.o ./Build/smoothnessterm_cpu.o   -ltiff -lgomp
     echo "--------------------------------------------------"
 else
-    echo "Cannot build without CUDA support!"
+    if [ ! $BUILD_BINARY ]
+    then
+        echo "Cannot build without CUDA support!"
+    fi
+fi
+
+if $UNZIP_DEMOS
+then
+    echo "unzipping ray demo..."
+    unzip -qo Demos/RayDemo/Frame01.zip -d Demos/RayDemo/
+    unzip -qo Demos/RayDemo/Frame02.zip -d Demos/RayDemo/
+    unzip -qo Demos/RayDemo/Frame03.zip -d Demos/RayDemo/
+    unzip -qo Demos/RayDemo/Mask01.zip -d Demos/RayDemo/
+    echo "--------------------------------------------------"
 fi
 
 ################################################################################################################################################################
