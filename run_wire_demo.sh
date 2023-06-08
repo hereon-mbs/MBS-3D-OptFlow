b2646072de60b2cd84db48e433923cd765ebe863
brunsst <91964829+brunsst@users.noreply.github.com>
2023-06-08 18:24:47 +0200

    Update run_wire_demo.sh

--- run_wire_demo.sh
+++ run_wire_demo.sh
@@ -17,6 +17,7 @@ GRADIENTMASK="-gradientmask 0 0.05"
 ADDITIONAL_ARGS="-prestrain_ref 0.00475 0.5"
 
 SIGMA_RENDERING=7
+STRAIN_OUTPUTS="Ezz"
 
 ROOTPATH="/Demos/WireDemo/"
 OUTPATH="/Demos/WireDemo/optflow/"
@@ -29,10 +30,11 @@ Frame06
 
 ############################# Execute #############################
 ARGUMENTS="-alpha "$ALPHA" -norm "$NORMALIZATION" -level "$LEVEL" -scale "$SCALE" -prefilter gaussian "$PREFILTER" -gpu0 "$GPU" -localglobal "$LOCALGLOBAL" "$ADDITIONAL_ARGS
+DISPLAY_ARGUMENTS="--taubin --vertices "$STRAIN_OUTPUTS" -sigma "$SIGMA_RENDERING
 
 for i in $EXPERIMENT_LIST; do
     INPATH=$i
     $DVCPROGRAM -i0 $ROOTPATH"/Frame00/" -i1 $ROOTPATH$INPATH -o $OUTPATH $ARGUMENTS
-    $ANALYSISPROGRAM -i_mesh /Demos/WireDemo/wire_mesh.vtk -i_disp $OUTPATH --taubin --vertices Ezz -sigma $SIGMA_RENDERING
+    $ANALYSISPROGRAM -i_mesh /Demos/WireDemo/wire_mesh.vtk -i_disp $OUTPATH $DISPLAY_ARGUMENTS
 done
 ###################################################################
